295d35a0e228aab040b7534a9f9e98523c2f21ae
MexX <mr.mexx@posteo.de>
2021-06-18 18:49:15 +0200

    erste Uebungen

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100 # 100m start height
+num_bounce = 1
+while num_bounce < 11:
+    height = round(height * 3/5, 4)
+    num_bounce = num_bounce + 1
+    print(num_bounce, end = ' ')
+    print(height)
