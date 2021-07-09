3c0644484ca3134d22896ddf82712b477c6a0b69
MexX <mr.mexx@posteo.de>
2021-07-09 17:21:37 +0200

    finished 'files' exercise

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,13 @@
 # pcost.py
 #
 # Exercise 1.27
+total_cost = 0.0
+
+with open('Data/portfolio.csv','rt') as f:
+    headers = next(f).split(',')
+    column_shares = headers.index('shares')
+    # column_price = headers.index('price')
+    for line in f:
+        row = line.split(',')
+        total_cost += int(row[column_shares]) * float(row[2])
+    print('Total cost',total_cost)
