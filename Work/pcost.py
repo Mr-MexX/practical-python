c7c57ce6ade28ca26e84afea439f4ac0fcfce3e2
MexX <mr.mexx@posteo.de>
2021-08-19 16:29:23 +0200

    exercise 1.32 (use csv library)

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,21 +1,24 @@
 # pcost.py
 #
-# Exercise 1.31
+# Exercise 1.32
 def portfolio_cost(filename):
     '''
     Computes the total cost (shares*price) of a portfolio file
     '''
     total_cost = 0.0
-
-    with open(filename, 'rt') as f:
-        headers = next(f).split(',')
-        for line in f:
-            try:
-                row = line.split(',')
-                total_cost += int(row[1]) * float(row[2])
-            except ValueError:
-                print("Couldn't parse", line)
+    import csv
+    f = open(filename)
+    rows = csv.reader(f)
+    headers = next(rows) # optional print
+    print(headers)
+    for row in rows:
+        try:
+            total_cost += int(row[1]) * float(row[2])
+            print(row) # optional print
+        except ValueError:
+            print("Couldn't parse", row)
     return total_cost
 
 cost = portfolio_cost('Data/missing.csv')
 print('Total cost', cost)
+
